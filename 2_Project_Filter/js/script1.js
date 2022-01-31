b74bd464ed9d5f58ea4d1e0ad4061b7d598b83b1
Tim FOURNIER <77847587+timfournier@users.noreply.github.com>
2022-01-31 15:36:30 +0100

    Add files via upload

---
+++ 2_Project_Filter/js/script1.js
@@ -0,0 +1,9 @@
+var el = document.querySelector('#filter');
+var el2 = document.querySelector('section');
+
+el.onclick = function() {
+  el2.classList.toggle('list');
+}
+
+
+
---
+++ 2_Project_Filter/js/script2.js
@@ -0,0 +1,5 @@
+var el = document.querySelector(".rotate");
+
+el.onclick = function(){
+    el.classList.toggle("down");
+}
